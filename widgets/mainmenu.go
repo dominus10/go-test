1f7441688dc6bd85c0249a0cf8fce29e52fb90c1
Nicholas <38601700+dominus10@users.noreply.github.com>
2024-12-24 08:01:13 +0900

    add sample bg

--- widgets/mainmenu.go
+++ widgets/mainmenu.go
@@ -4,12 +4,16 @@ import (
 	"game/injection"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func MainMenuWidget(manager injection.ScreenMan) fyne.CanvasObject {
+	img:= canvas.NewImageFromFile("./assets/bg.jpg")
+	img.FillMode = canvas.ImageFillStretch
+
 	// Create buttons
 	continueButton := widget.NewButton("Continue", func() {})
 	newGameButton := widget.NewButton("New Game", func() {})
@@ -23,6 +27,7 @@ func MainMenuWidget(manager injection.ScreenMan) fyne.CanvasObject {
 	// Use a vertical box layout with a spacer above the buttons to push them to the bottom
 	bottomLayout := container.New(layout.NewVBoxLayout(),layout.NewSpacer(),composed,spacer)
 	ctx := container.New(layout.NewHBoxLayout(),layout.NewSpacer(), bottomLayout,layout.NewSpacer())
+	stack := container.NewStack(img, ctx)
 
-	return ctx
+	return stack
 }
